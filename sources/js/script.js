49e100fec68034cf42997f6580fc934bb77d4095
kadekdarmayasa <darmayasadiputra@gmail.com>
2022-06-18 20:59:20 +0800

    Finishing all sections and make the aside position be stikcy

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,25 +1,39 @@
-const toggler = document.getElementById('toggler');
-const navbarLink = document.querySelector('.navbar-link');
+(() => {
+	const toggler = document.getElementById('toggler');
+	const navLinkContainer = document.querySelector('.navbar-link-container');
 
-toggler.addEventListener('click', () => {
-	navbarLink.classList.toggle('showNav');
-});
+	toggler.addEventListener('click', () => {
+		navLinkContainer.classList.toggle('showNav');
+	});
 
-window.addEventListener('scroll', () => {
-	const navbar = document.querySelector('nav');
-	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-		navbar.style.boxShadow = '0px 2px 10px rgba(255, 191, 156, 0.4)';
-	} else {
-		navbar.style.boxShadow = 'none';
+	window.addEventListener('scroll', () => {
+		const navbar = document.querySelector('nav');
+		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+			navbar.style.boxShadow = '0px 2px 10px rgba(255, 191, 156, 0.4)';
+		} else {
+			navbar.style.boxShadow = 'none';
+		}
+
+		activeLink();
+	});
+
+	const navLinkContents = document.querySelectorAll('.nav-link-content');
+	const navLinkItems = navLinkContainer.querySelectorAll('.link-item');
+	function activeLink() {
+		let current = '';
+
+		navLinkContents.forEach((content) => {
+			const sectionTop = content.offsetTop;
+			if (scrollY >= sectionTop - 250) {
+				current = content.getAttribute('id');
+			}
+		});
+
+		navLinkItems.forEach((item) => {
+			item.classList.remove('active');
+			if (item.classList.contains(current)) {
+				item.classList.add('active');
+			}
+		});
 	}
-});
-
-let parent = document.querySelector('aside .card').parentElement;
-
-while (parent) {
-	const hasOverflow = getComputedStyle(parent).overflow;
-	if (hasOverflow !== 'visible') {
-		console.log(hasOverflow, parent);
-	}
-	parent = parent.parentElement;
-}
+})();
